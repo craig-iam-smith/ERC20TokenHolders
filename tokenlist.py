df97399ea603166dddef7564b4d0677a9b632fe9
craig-iam-smith <craig.iam.smith@gmail.com>
2023-01-08 19:43:08 -0700

    fix up the column names

--- tokenlist.py
+++ tokenlist.py
@@ -6,10 +6,10 @@ totals = defaultdict(float)
 with open('export-token.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
-        row['column_6'] = row['column_6'].replace(",","")
-        if row['column_7'] in ('Transfer', '0x60806040'):
-            totals[row['column_4']] -= float(row['column_6'])
-            totals[row['column_5']] = totals.setdefault(row['column_5'], 0.0) + float(row['column_6'])
+        row['Quantity'] = row['Quantity'].replace(",","")
+        if row['Method'] in ('Transfer', '0x60806040'):
+            totals[row['From']] -= float(row['Quantity'])
+            totals[row['To']] = totals.setdefault(row['To'], 0.0) + float(row['Quantity'])
 
 for x in totals:
     if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
