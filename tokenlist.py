5b7612aee6a9ee18f9860cf69832a9f7d168dbb8
craig-iam-smith <craig.iam.smith@gmail.com>
2023-02-15 15:57:22 -0700

    add JSON output

--- tokenlist.py
+++ tokenlist.py
@@ -10,7 +10,11 @@ with open('export-token.csv', newline='') as csvfile:
         if row['Method'] in ('Transfer', '0x60806040'):
             totals[row['From']] -= float(row['Quantity'])
             totals[row['To']] = totals.setdefault(row['To'], 0.0) + float(row['Quantity'])
-
+print('[')
 for x in totals:
     if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
-        print(x, totals[x])
+        print('     {')
+        print('     "address": ' + x + ',')
+        print('     "balance": ' + str(totals[x]))
+        print('     },')
+print(']')
