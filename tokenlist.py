47f9c5ffa4ef3dc297832f99081a81c377bfeeff
craig-iam-smith <craig.iam.smith@gmail.com>
2023-02-16 15:39:08 -0700

    write both json and text file

--- tokenlist.py
+++ tokenlist.py
@@ -10,11 +10,18 @@ with open('export-token.csv', newline='') as csvfile:
         if row['Method'] in ('Transfer', '0x60806040'):
             totals[row['From']] -= float(row['Quantity'])
             totals[row['To']] = totals.setdefault(row['To'], 0.0) + float(row['Quantity'])
-print('[')
-for x in totals:
-    if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
-        print('     {')
-        print('     "address": ' + x + ',')
-        print('     "balance": ' + str(totals[x]))
-        print('     },')
-print(']')
+with open("tokenlist.json", "w") as text_file:
+    text_file.write("[\n")
+    for x in totals:
+        if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
+            text_file.write('     {\n')
+            text_file.write('     "address": ' + x + ',\n')
+            text_file.write('     "balance": ' + str(totals[x])+ '\n')
+            text_file.write('     },\n')
+    text_file.write(']\n')
+
+with open("tokenlist.txt", "w") as text_file:
+    for x in totals:
+        if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
+            text_file.write(x + ',' + str(totals[x]) + "\n");
+
