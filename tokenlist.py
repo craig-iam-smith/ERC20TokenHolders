15d7dd68c53be6d1715cc1c30c55c9663e328f8e
craig-iam-smith <craig.iam.smith@gmail.com>
2023-03-08 14:31:13 -0700

    update for format change on etherscan

--- tokenlist.py
+++ tokenlist.py
@@ -3,25 +3,18 @@ from collections import defaultdict
 
 totals = defaultdict(float)
 
-with open('export-token.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    for row in reader:
-        row['Quantity'] = row['Quantity'].replace(",","")
-        if row['Method'] in ('Transfer', '0x60806040'):
-            totals[row['From']] -= float(row['Quantity'])
-            totals[row['To']] = totals.setdefault(row['To'], 0.0) + float(row['Quantity'])
 with open("tokenlist.json", "w") as text_file:
     text_file.write("[\n")
-    for x in totals:
-        if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
+    with open('export-token.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            total = float(row['Balance'])
+            total = total / 100000000
+            print(row['HolderAddress'], total)
             text_file.write('     {\n')
-            text_file.write('     "address": ' + x + ',\n')
-            text_file.write('     "balance": ' + str(totals[x])+ '\n')
+            text_file.write('     "address": ' + "\"" + row['HolderAddress']+"\"" + ',\n')
+            text_file.write('     "balance": ' + "\"" + str(total)+ "\"" + '\n')
             text_file.write('     },\n')
-    text_file.write(']\n')
+        text_file.write(']\n')
 
-with open("tokenlist.txt", "w") as text_file:
-    for x in totals:
-        if x != '0x0000000000000000000000000000000000000000' and totals[x] != 0:
-            text_file.write(x + ',' + str(totals[x]) + "\n");
 
